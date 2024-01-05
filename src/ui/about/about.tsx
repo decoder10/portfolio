00a26f9af5b37e9de270dc48beefa1f76e31f035
Aram Mkrtchyan <aram.mkrtchyan@betconstruct.com>
2024-01-05 12:03:12 +0400

    fixes

--- src/ui/about/about.tsx
+++ src/ui/about/about.tsx
@@ -6,9 +6,9 @@ const About: FC = () => {
   return (
     <div className={styles.about}>
       <p>
-        Experienced Senior Front-End Developer with a decade of expertise in the banking, gambling, and digital wallet
-        industries. Proficient in JavaScript, React, React Native, CSS, and HTML, including Css preprocessors.
-        Specialized in creating mobile banking apps, admin panels, and casino platforms, with a strong focus on
+        Experienced Senior Front-End Developer with a decade of expertise in the banking, gambling and digital wallet
+        industries. Proficient in JavaScript, React, React Native, CSS and HTML, including CSS preprocessors.
+        Specialized in creating mobile banking apps, admin panels and casino platforms, with a strong focus on
         user-centric design and functionality. Recognized for delivering high-quality solutions and contributing to
         successful projects.
       </p>
