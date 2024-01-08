12fcc4484033a8a490749cecc28caabd6faf6dfd
Aram Mkrtchyan <aram.mkrtchyan@betconstruct.com>
2024-01-08 12:01:10 +0400

    css fix

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -55,15 +55,17 @@ const Header = (props: IProps) => {
         </nav>
       </header>
 
-      <div className={`${styles.thunderGroup} ${menuState ? styles.active : ''}`}>
-        <div className={styles.thunder} />
-        <div className={styles.thunderSecond} />
-      </div>
-
       <button className={`${styles.menu} ${!menuState ? styles.active : ''}`} onClick={() => setMenuState(!menuState)}>
         <span></span>
         <span></span>
       </button>
+
+      {dayState === 'dark-theme' ? (
+        <div className={`${styles.thunderGroup} ${menuState ? styles.active : ''}`}>
+          <div className={styles.thunder} />
+          <div className={styles.thunderSecond} />
+        </div>
+      ) : null}
     </>
   );
 };
