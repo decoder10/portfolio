d98ef7f47be57f0339a989000b54185ed1ac9a24
Aram Mkrtchyan <aram.mkrtchyan@betconstruct.com>
2024-01-10 11:25:46 +0400

    fixes

--- src/config/store.ts
+++ src/config/store.ts
@@ -1,9 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { rootReducer } from 'reducers';
-import logger from 'redux-logger';
+// import logger from 'redux-logger';
 
 export const store = configureStore({
   reducer: rootReducer,
-  middleware: getDefaultMiddleware => getDefaultMiddleware().concat(logger),
+  // middleware: getDefaultMiddleware => getDefaultMiddleware().concat(logger),
   devTools: process.env.NODE_ENV !== 'production',
 });
